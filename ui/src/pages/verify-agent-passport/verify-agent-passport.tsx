c4bcca266c523cc77bdbf459d8538209a671bb2c
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-05-05 13:39:48 +0100

    fix: refactor style uI

--- ui/src/pages/verify-agent-passport/verify-agent-passport.tsx
+++ ui/src/pages/verify-agent-passport/verify-agent-passport.tsx
@@ -1,5 +1,6 @@
 import {BasePage} from '@/components/layout/base-page';
 import PlaceholderPageContent from '@/components/ui/placeholder-page-content';
+import {Link} from 'react-router-dom';
 
 const VerifyAgentPassport: React.FC = () => {
   return (
@@ -8,7 +9,13 @@ const VerifyAgentPassport: React.FC = () => {
       description={
         <div className="space-y-4">
           <p>You can copy an Agent Passport into the field below, and verify that is legitimate and linked to a trust anchor. </p>
-          <p>You can read more about the Internet of Agents trust anchors here.</p>
+          <p>
+            You can read more about the Internet of Agents trust anchors{' '}
+            <Link to="#" className="inline-link">
+              here
+            </Link>
+            .
+          </p>
         </div>
       }
       useBreadcrumbs={false}
