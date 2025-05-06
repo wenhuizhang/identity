83083482f604f285f0ff7815b28329dbac5db27d
copasseron <cpassero@cisco.com>
2025-05-06 17:09:13 +0200

    feat: define keygen interface

---
+++ identity/internal/core/keygen/key_service.go
@@ -0,0 +1,22 @@
+package keygen
+
+import (
+	"context"
+
+	"github.com/agntcy/identity/internal/core/id/types"
+)
+
+// KeyService defines methods for generating, saving, and retrieving JWKs.
+type KeyService interface {
+	// GenerateKey generates and save a new JWK.
+	GenerateKey(ctx context.Context, alg string, use string, id string) (*types.Jwk, error)
+
+	// SaveKey saves a JWK to the key storage.
+	SaveKey(ctx context.Context, id string, jwk *types.Jwk) error
+
+	// RetrieveKey retrieves a public JWK by its ID.
+	RetrievePubKey(ctx context.Context, id string) (*types.Jwk, error)
+
+	// RetrieveKey retrieves a private JWK by its ID.
+	RetrievePrivKey(ctx context.Context, id string) (*types.Jwk, error)
+}
