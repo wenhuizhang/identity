7b681070a87f3f4afade83b2d8d80c4e94d2d96a
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-05-07 14:33:27 +0100

    fix: refactor code

--- ui/src/types/router.ts
+++
@@ -1,7 +0,0 @@
-import {RouteObject} from 'react-router-dom';
-
-export interface CustomRoute {
-  disabled?: boolean;
-}
-
-export type Route = RouteObject & CustomRoute;
--- ui/src/types/wallet-providers.ts
+++
@@ -1,9 +0,0 @@
-export enum WalletProviders {
-  ONE_PASSWORD = '1password',
-  BIT_WARDEN = 'bitwarden',
-  DROPBOX = 'dropbox',
-  PROTON_PASSWORD = 'proton-password',
-  DASH_LANE = 'dashlane',
-  KEEPER = 'keeper',
-  ZOHO = 'zoho'
-}
