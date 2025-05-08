424d6aa69aef0fe528e94f27e6de3c80f0a062f6
jdiaconu <jdiaconu@cisco.com>
2025-05-08 15:43:50 +0200

    feat: Docker cleanup + fixes on sh

--- deployments/scripts/docs/stop_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2025  AGNTCY Contributors (https://github.com/agntcy)
-# SPDX-License-Identifier: Apache-2.0
-
-
-set -Eeuo pipefail
-
-docker compose -f ./deployments/docker-compose/docs/docker-compose.yml down
