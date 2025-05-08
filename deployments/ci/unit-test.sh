f3e58be842299e26a821a6422fee77aaac937816
jdiaconu <jdiaconu@cisco.com>
2025-05-08 18:07:11 +0200

    fix: Fix super-linter

--- deployments/ci/unit-test.sh
+++ deployments/ci/unit-test.sh
@@ -7,4 +7,4 @@ DOCKER_FILE=./deployments/docker/identity/Dockerfile.test
 TEST_COMMAND='go test -coverprofile=/mnt/coverage.out ./...'
 
 echo RUNNING TESTS
-docker run -v "${PWD}":/mnt "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" $TEST_COMMAND
+docker run -v "${PWD}":/mnt "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" "$TEST_COMMAND"
