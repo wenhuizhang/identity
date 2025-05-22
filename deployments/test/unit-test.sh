48f4a21c94d44e743a9d5963e9e24bc27dc2fa06
jdiaconu <jdiaconu@cisco.com>
2025-05-22 10:26:47 +0200

    feat: Update test scripts

--- deployments/test/unit-test.sh
+++ deployments/test/unit-test.sh
@@ -4,7 +4,7 @@
 
 
 DOCKER_FILE=./deployments/docker/identity/Dockerfile.test
-TEST_COMMAND='go test -coverprofile=/mnt/coverage.out ./...'
+TEST_COMMAND='go test -v -cover ./...'
 
 echo RUNNING TESTS
-docker run -v "${PWD}":/mnt "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" "$TEST_COMMAND"
+docker run "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" "$TEST_COMMAND"
