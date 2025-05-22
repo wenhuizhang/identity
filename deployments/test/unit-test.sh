07b7728badc218da7cd35270742202888c0411ae
jdiaconu <jdiaconu@cisco.com>
2025-05-22 10:01:49 +0200

    feat: Move to test

---
+++ deployments/test/unit-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright 2025 AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+
+DOCKER_FILE=./deployments/docker/identity/Dockerfile.test
+TEST_COMMAND='go test -coverprofile=/mnt/coverage.out ./...'
+
+echo RUNNING TESTS
+docker run -v "${PWD}":/mnt "$(docker build --no-cache -f ${DOCKER_FILE} -q .)" "$TEST_COMMAND"
