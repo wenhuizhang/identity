7525e77dec476134a198f28e5e5946ed5ff302ad
jdiaconu <jdiaconu@cisco.com>
2025-05-23 10:30:28 +0200

    feat: Add MCP implem

---
+++ samples/mcp/main.py
@@ -0,0 +1,62 @@
+# Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+
+import httpx
+import uvicorn
+from fastapi import FastAPI
+
+from mcp.server.fastmcp import FastMCP
+
+logging.basicConfig(level=logging.INFO)
+
+##### MCP Tools and Resources #####
+
+mcp = FastMCP("GitHub", stateless_http=True)
+
+
+@mcp.tool()
+def get_exchange_rate(
+    currency_from: str = "USD",
+    currency_to: str = "EUR",
+    currency_date: str = "latest",
+):
+    """Use this to get current exchange rate.
+
+    Args:
+        currency_from: The currency to convert from (e.g., "USD").
+        currency_to: The currency to convert to (e.g., "EUR").
+        currency_date: The date for the exchange rate or "latest". Defaults to "latest".
+
+    Returns:
+        A dictionary containing the exchange rate data, or an error message if the request fails.
+    """
+    try:
+        response = httpx.get(
+            f"https://api.frankfurter.app/{currency_date}",
+            params={"from": currency_from, "to": currency_to},
+        )
+        response.raise_for_status()
+
+        data = response.json()
+        if "rates" not in data:
+            return {"error": "Invalid API response format."}
+        return data
+    except httpx.HTTPError as e:
+        return {"error": f"API request failed: {e}"}
+    except ValueError:
+        return {"error": "Invalid JSON response from API."}
+
+
+##### MCP Tools and Resources #####
+
+##### Server and Middlewares #####
+
+app = FastAPI(lifespan=lambda _: mcp.session_manager.run())
+app.mount("/", mcp.streamable_http_app())
+
+##### Server and Middlewares #####
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
