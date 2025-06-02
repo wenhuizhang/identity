3d8b68c8fd5ad34ee54c6987043057a9e320ade3
Rafael Silva (rafaelsi) <rafaelsi@cisco.com>
2025-06-02 12:21:39 +0100

    feat (cli): add links stdout issuer and metada

--- cmd/issuer/commands/issuer/register.go
+++ cmd/issuer/commands/issuer/register.go
@@ -148,7 +148,8 @@ func (cmd *RegisterCommand) Run(ctx context.Context, flags *RegisterFlags) error
 	)
 	fmt.Fprintf(
 		os.Stdout,
-		"\nYou can now access the Issuer's Well-Known Public Key at: http://localhost:4000/v1alpha1/issuer/%s/.well-known/jwks.json\n",
+		"\nYou can now access the Issuer's Well-Known Public Key at: "+
+			"http://localhost:4000/v1alpha1/issuer/%s/.well-known/jwks.json\n",
 		commonName,
 	)
 
