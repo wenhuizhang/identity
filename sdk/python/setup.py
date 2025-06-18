f4de8c87bfab7b4348d216f60c6b9e86f045d397
jdiaconu <jdiaconu@cisco.com>
2025-06-18 11:33:45 +0200

    feat: Add deps
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

---
+++ sdk/python/setup.py
@@ -0,0 +1,12 @@
+# Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+"""Setup script for the package."""
+
+import setuptools
+
+
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    setuptools.setup()
