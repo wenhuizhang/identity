7c2b079a3d926f4de776b41ad026f9b951a0a469
jdiaconu <jdiaconu@cisco.com>
2025-06-18 18:19:10 +0200

    fix: Fix linter
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,11 +1,9 @@
+#!/usr/bin/env python
 # Copyright 2025 Copyright AGNTCY Contributors (https://github.com/agntcy)
 # SPDX-License-Identifier: Apache-2.0
 """Setup script for the package."""
 
 import setuptools
-
-
-#!/usr/bin/env python
 
 if __name__ == "__main__":
     setuptools.setup()
